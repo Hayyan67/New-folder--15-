2bd9f9066df49cf8245d36693b218b4f847908fc
Hayyan67 <humairabaig81@gmail.com>
2024-11-10 10:49:39 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+l1=(2, 4, 5, 12, 423, 56, 4, 4)
+print(type(l1))
+print(l1[2])
+print(l1[1:5])
+print(len(l1))
+print(min(l1))
+print(max(l1))
+print(l1.count(4))
+l2=list(l1)
+12[4]=77
+print(12)
+l1=tuple(12)
+print(l1)
